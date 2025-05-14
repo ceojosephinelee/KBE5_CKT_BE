de8e58dd0c8ab6d82a6f8b5997274847795d2638
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-05-14 15:27:42 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/CKT_BE/CktBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CKT_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CktBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CktBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CKT_BE/CktBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CKT_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CktBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
